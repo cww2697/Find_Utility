b06baac3fb1b796c482ae8bfe5cb0b63ad20ea64
Cody West <cww2697@gmail.com>
2020-04-26 16:28:25 -0500

    Update to find_util.c

--- find_util.c
+++ find_util.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
+#include <sys/stat.h>
 
 void find_args(const char *path, int nflag, const char* name, int iflag, int inode_num int mflag, int mins, int rflag, int eflag);
 void find(const char *path);
@@ -11,15 +12,20 @@ int main(int argc, char **argv)
     char *dir, *name;
     int inode_num, mins, nflag, iflag, mflag, rflag, eflag;
     dir = argv[1];
-    
-    if(argc <2){
-	    dir=".";
+    if (argc <3){
+        if(argc <2){
+	        dir=".";
+        }
+        else{
+	        dir=argv[1];	
+        }
+    }
+        find(dir);
     }
     else{
-	    dir=argv[1];	
+        /*code for switch*/
     }
-    finds(dir);
-
+    
     return 0;
 }
 
@@ -59,15 +65,36 @@ void find(const char *basePath)
 }
 
 void find_args(const char* path, int nflag, const char* name, int iflag, int inode_num, int mflag, int mins int rflag){
-    if (nflag == 1){}
-    if (iflag == 1){}
-    if (mflag == 1){}
-    if (rflag == 1){}
-    if (eflag == 1){}
-    else{
-        break;  //Exception handling, since if all these are false code should not direct to this find function
+    
+    /*Checks all flags to see which flag is used and runs the proper code for
+    the corresponding flag i.e. iflag == 1, find file with inode_num == inode*/
+
+    if (nflag == 1){
+        /*statement*/
     }
-
-
-
+    else{
+        if (iflag == 1){
+            /*statement*/
+        }
+        else{
+            if (mflag == 1){
+                /*statement*/
+            }
+            else{
+                if (rflag == 1){
+                    /*statement*/
+                }
+                else{
+                    if (eflag == 1){
+                        /*statement*/
+                    }
+                    else{
+                        /*Exception handling, since if all these are false code should not 
+                        direct to this find function*/
+                        break;
+                    }
+                } 
+            }
+        }
+    }
 }
