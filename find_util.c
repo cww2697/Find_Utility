7320490b40493d13c8da2e7d6c807a28a4fa9991
Cody West <cww2697@gmail.com>
2020-05-01 11:54:33 -0500

    Update find_util.c
    Update code to add find by name functionality

--- find_util.c
+++ find_util.c
@@ -1,31 +1,79 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <string.h>
-#include <sys/stat.h>
+#include <stdarg.h>
+#include <unistd.h>
 
-void find_args(const char *path, int nflag, const char* name, int iflag, int inode_num int mflag, int mins, int rflag, int eflag);
-void find(const char *path);
+
+void find(const char *path, int n, const char *name, int m, int mmin, int i, const char *inum);
 
 int main(int argc, char **argv)
 {
-    char *dir, *name;
-    int inode_num, mins, nflag, iflag, mflag, rflag, eflag;
+    // Directory path to list files
+   /* char *dir;
     dir = argv[1];
-    if (argc <3){
-        if(argc <2){
-	        dir=".";
-        }
-        else{
-	        dir=argv[1];	
-        }
-    }
-        find(dir);
+    
+    if(argc <2){
+	    dir=".";
     }
     else{
-        /*code for switch*/
+	    dir=argv[1];	
     }
-    
+    find(dir);*/
+
+    int		w, n, m, i, a, mmin;
+	char  *where, *name, *mmin_string, *inum, *action;
+	while (1) {
+		char		c;
+
+	
+
+		c = getopt(argc, argv, "w:n:m:i:a:");	
+
+		if (c == -1) {
+			break;
+		}
+		switch (c) {
+		case 'w':
+			w = 1;
+			where = optarg;
+			//printf("where: %s\n", optarg);
+			break;
+		case 'n':
+			n = 1;
+			name = optarg;
+			//printf("name: %s\n", optarg);
+			break;
+		case 'm':
+			m = 1;
+			mmin_string = optarg;
+			mmin = atoi(mmin_string);
+			//printf("mmin: %d\n", mmin);
+			break;
+		case 'i':
+			i = 1;
+			inum = optarg;
+			//printf("inum: %s\n", optarg);
+			break;
+		case 'a':
+			a = 1;
+			action = optarg;
+			//printf("action: %s\n", optarg);
+			break;
+		case '?':
+		default:
+			printf("An invalid option detected.\n");
+		}
+	}
+
+    if(w==0){
+		where = ".";
+	}
+
+    find(where, n , name, m, mmin);
+
     return 0;
 }
 
@@ -33,12 +81,14 @@ int main(int argc, char **argv)
 /**
  * Lists all files and sub-directories at given path.
  */
-void find(const char *basePath)
+void find(const char *basePath, int n, const char *name)
 {
     char path[1000];
     struct dirent *dp;
     DIR *dir = opendir(basePath);
-
+    //printf("%d %s\n", n ,name);
+    
+    
     // Unable to open directory stream
     if (!dir){
         return;
@@ -48,53 +98,50 @@ void find(const char *basePath)
     {
         if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
         {
-            //printf("%s\n", dp->d_name);
-            //printf("%s\n", path);
-            // Construct new path from our base path
+            if(n==1){
+                if(strcmp(dp->d_name, name) == 0){
+                    if(m==1){
+
+                    }
+                    else{
+
+                    }
+                    strcpy(path, basePath);
+                    strcat(path, "/");
+                    strcat(path, dp->d_name);
+                    //printf("file name: %s\n", dp->d_name);
+                    //printf("Original path: %s\n", path); 
+                    printf("%s\n", path);   
+            
+                }
+            }
+            else{
+                if(m==1){
+                    
+                }
+                 else{
+                        
+                }
+                //printf("file name: %s\n", dp->d_name);
+                //printf("Original path: %s\n", path); 
+                strcpy(path, basePath);
+                strcat(path, "/");
+                strcat(path, dp->d_name);
+                printf("%s\n", path);   
+            
+            }
+
             strcpy(path, basePath);
             strcat(path, "/");
             strcat(path, dp->d_name);
-            printf("%s\n", path);   
+            find(path, n, name);
+            //printf("%s\n", dp->d_name);
+            //printf("%s\n", path);
+            // Construct new path from our base path
             
-            listFiles(path);
             
         }
     }
 
     closedir(dir);
-}
-
-void find_args(const char* path, int nflag, const char* name, int iflag, int inode_num, int mflag, int mins int rflag){
-    
-    /*Checks all flags to see which flag is used and runs the proper code for
-    the corresponding flag i.e. iflag == 1, find file with inode_num == inode*/
-
-    if (nflag == 1){
-        /*statement*/
-    }
-    else{
-        if (iflag == 1){
-            /*statement*/
-        }
-        else{
-            if (mflag == 1){
-                /*statement*/
-            }
-            else{
-                if (rflag == 1){
-                    /*statement*/
-                }
-                else{
-                    if (eflag == 1){
-                        /*statement*/
-                    }
-                    else{
-                        /*Exception handling, since if all these are false code should not 
-                        direct to this find function*/
-                        break;
-                    }
-                } 
-            }
-        }
-    }
 }
